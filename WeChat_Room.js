37cf88d2bda31a23d14d9227536af70717cb1c29
Gaurik-Gupta <125227706+Gaurik-Gupta@users.noreply.github.com>
2023-10-22 18:51:44 -0400

    Add files via upload

---
+++ WeChat_Room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCefGLPxLIXe_z2EwAyhLRlVizaSeYXMGw",
+    authDomain: "wechat-b888a.firebaseapp.com",
+    projectId: "wechat-b888a",
+    storageBucket: "wechat-b888a.appspot.com",
+    messagingSenderId: "287018980880",
+    appId: "1:287018980880:web:a743b27c41c8a16f381c95"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
